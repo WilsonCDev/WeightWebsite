3e794b57a1e1eba6e88b26d483ba7a878114cfdf
cwilson15205 <cwilson15205@gmail.com>
2017-01-15 17:31:43 -0500

    Update so I can remove the VCS of config

---
+++ lib/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("HOST", "localhost");
+define("USER", "");
+define("PASS", "");
+define("DB", "");
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE);
